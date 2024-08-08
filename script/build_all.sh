9a6edafd7f6ac413784827069bcba738d20d724a
MXY156018 <1132022296@qq.com>
2024-08-08 23:19:11 +0800

    更新

--- script/build_all.sh
+++ script/build_all.sh
@@ -15,10 +15,10 @@ echo ${serviceDir}
 
 cd ../service/admin && go build -ldflags="-w -s" -o mall_admin && mv mall_admin ${binDir}   && cd ${rootDir}
 
-cd ../service/product && go build -ldflags="-w -s" -o mall_product && mv mall_product ${binDir}   && cd ${rootDir}
+# cd ../service/product && go build -ldflags="-w -s" -o mall_product && mv mall_product ${binDir}   && cd ${rootDir}
 
-cd ../service/user && go build -ldflags="-w -s" -o mall_user && mv mall_user ${binDir}   && cd ${rootDir}
+# cd ../service/user && go build -ldflags="-w -s" -o mall_user && mv mall_user ${binDir}   && cd ${rootDir}
 
-cd ../service/order && go build -ldflags="-w -s" -o mall_order && mv mall_order ${binDir}   && cd ${rootDir}
+# cd ../service/order && go build -ldflags="-w -s" -o mall_order && mv mall_order ${binDir}   && cd ${rootDir}
 
-cd ../service/message && go build -ldflags="-w -s" -o mall_message && mv mall_message ${binDir}   && cd ${rootDir}
+# cd ../service/message && go build -ldflags="-w -s" -o mall_message && mv mall_message ${binDir}   && cd ${rootDir}
--- script/start_all.sh
+++ script/start_all.sh
@@ -17,26 +17,26 @@ echo '启动管理员服务'
 nohup ./mall_admin > mall_admin.out 2>&1 &
 sleep 3
 
-echo '启动用户服务'
-nohup ./mall_user > mall_user.out 2>&1 &
-sleep 3
+# echo '启动用户服务'
+# nohup ./mall_user > mall_user.out 2>&1 &
+# sleep 3
 
-echo '启动商品服务'
-nohup ./mall_product > mall_product.out 2>&1 &
-sleep 3
+# echo '启动商品服务'
+# nohup ./mall_product > mall_product.out 2>&1 &
+# sleep 3
 
-echo '启动订单服务'
-nohup ./mall_order > mall_order.out 2>&1 &
-sleep 3
+# echo '启动订单服务'
+# nohup ./mall_order > mall_order.out 2>&1 &
+# sleep 3
 
-echo '启动消息服务'
-nohup ./mall_message > mall_message.out 2>&1 &
-sleep 3
+# echo '启动消息服务'
+# nohup ./mall_message > mall_message.out 2>&1 &
+# sleep 3
 
 ps -aux | grep mall_admin
-ps -aux | grep mall_product
-ps -aux | grep mall_user
-ps -aux | grep mall_order
-ps -aux | grep mall_message
+# ps -aux | grep mall_product
+# ps -aux | grep mall_user
+# ps -aux | grep mall_order
+# ps -aux | grep mall_message
 
 cd -
